336c53bb7289db15aafd62530e08db525754d62a
Vitor Campione <vitor.campione@etec.sp.gov.br>
2023-09-05 14:06:20 -0300

    .

--- pages/empresa/index.php
+++ pages/empresa/index.php
@@ -3,3 +3,22 @@
     <a class="navbar-brand" href="#">Lista de Empresas</a>
   </div>
 </nav>
+
+
+
+<div class="container-fluid">
+    <table class="table"> 
+          <thead> 
+               <tr>
+                 <th scope="col">Nome da Empresa</th>
+                 <th scope="col">Telefone</th>
+               </tr> 
+          </thead>  
+          <tbody>
+             <tr>
+                <td><a href="#">ETEC Pedro Badran</a></td>
+                <td >(16)3818-2196</td>
+             </tr>
+          </tbody>  
+    </table>    
+</div>    
