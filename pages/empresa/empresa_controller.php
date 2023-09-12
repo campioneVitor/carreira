2c6a53d8e65d24f984add179825d43acc96c060b
Vitor Campione <vitor.campione@etec.sp.gov.br>
2023-09-12 13:06:01 -0300

    controller empresa

---
+++ pages/empresa/empresa_controller.php
@@ -0,0 +1,26 @@
+<?php
+           require_once "../dominio/empresa.php";
+    
+    class EmpresaController{
+
+           function incluir(Empresa $empresa){
+
+          }
+           function listar(){
+
+          }
+           function mostrar($id){
+
+          }
+           function atualizar(Empresa $empresa){
+
+          }
+
+          function deletar($id){
+
+          }
+
+
+    }
+
+?>
